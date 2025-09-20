e4ae321ee08458be3bd04437277717cc1438e8ac
Emin Isaev <emin.isaev97@gmail.com>
2025-09-20 23:32:01 +0700

    refactor: showcase get method of collection instance

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -3,8 +3,10 @@ import { Games } from '@/lib/api';
 import Link from 'next/link';
 
 export default async function Home() {
-  const games = await Games.list();
-  console.log(games);
+  const allGames = await Games.list();
+  const gameById = await Games.get('68ced14d0039e9e86110');
+  console.log('all games: ', allGames);
+  console.log('gameById: ', gameById);
 
   return (
     <div>
