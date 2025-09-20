d99835aa5939c747a3853386c36b350243649a94
Emin Isaev <emin.isaev97@gmail.com>
2025-09-20 23:19:29 +0700

    chore: integrate appwrite

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,9 +1,11 @@
-'use client';
-
 import { Text } from '@/components';
+import { Games } from '@/lib/api';
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const games = await Games.list();
+  console.log(games);
+
   return (
     <div>
       <Text className='mt-20 text-center' variant='h1'>
